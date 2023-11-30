37a91dc626ff2a8eb25af5f0b7860b1f5ba7da8c
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-29 22:08:57 -0800

    attempt at 1 activation event listener

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ function buttonClick() {
 }
  let button = document.querySelector("button");
  button.addEventListener("click", buttonClick);
+ if (buttonClick() = 1) {
+    button.removeEventListener("click", buttonClick)
+ }
