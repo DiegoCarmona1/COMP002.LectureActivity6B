942a5cb9a100879101701c147bcc21e4f5c0bd13
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-29 22:54:22 -0800

    added prevent default event listener to link

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ function buttonClick() {
 }
  let button = document.querySelector("button");
  button.addEventListener("click", buttonClick);
- 
+let link = document.querySelector("a");
+link.addEventListener("click", event => {
+    event.preventDefault();
+    alert("No DIstractions! Im Coding!");
+});
